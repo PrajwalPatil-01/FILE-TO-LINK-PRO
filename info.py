fa2c412886c06f3e73faf8ef63403005b8350dfa
PrajwalPatil-01 <prajwalpatilxp@gmail.com>
2026-02-02 13:34:31 +0530

    Update DB_URL to use MongoDB connection string
    Signed-off-by: PrajwalPatil-01 <prajwalpatilxp@gmail.com>

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ OWNER_USERNAME = environ.get("OWNER_USERNAME", 'xp_prajwal')
 # =========================================================
 # 🗄️ DATABASE CONNECTION
 # =========================================================
-DB_URL = environ.get('DATABASE_URI', "")
+DB_URL = environ.get('DATABASE_URI', "mongodb+srv://XP_Bot:XP_Bot@cluster0.kmhq8aq.mongodb.net/?appName=Cluster0")
 DB_NAME = environ.get('DATABASE_NAME', "Cluster0")
 
 # =========================================================
